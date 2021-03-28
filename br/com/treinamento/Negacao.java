73e45e56a711c6b696c7e653bed92fbde7d05250
Diego Brito <dibbrito@gmail.com>
2021-03-28 15:09:58 -0300

    Adc Navegacao

---
+++ br/com/treinamento/Negacao.java
@@ -0,0 +1,11 @@
+package br.com.treinamento;
+
+public class Negacao {
+    
+    public static void main(String[] args) {
+        boolean valido = true;
+        boolean invalido = !valido;
+    
+        System.out.println("Valor é " +  invalido);
+    }
+}
